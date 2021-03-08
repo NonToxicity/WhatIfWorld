9f5406ca0d8097f42f1d52941ea590c86c7d644b
uygurugurlu <uygurugurlu@gmail.com>
2021-03-08 07:11:36 +0300

    changes

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import pandas as pd
 from datetime import datetime as dt
 
 data = pd.read_csv('data.csv', names=['s', 'e', 'm']).set_index('m')
-series = pd.Series(index=range(data.s.min(), dt.now().year))
+
+series = pd.Series(index=range(data.s.min(), dt.now().year + 1))
 for m in data.index:
     series.loc[data.loc[m].s:data.loc[m].e] = m
 
@@ -17,4 +18,4 @@ def home():
     try:
         return series.loc[year]
     except KeyError:
-        return f'Invalid input'
+        return f'Invalid input ({series.index.min()} - {series.index.max()})'
