9a692199967b27ec3a73d24405861dab40de3f6d
uygurugurlu <uygurugurlu@gmail.com>
2021-08-29 00:11:51 +0300

    eslint changes

--- app.py
+++ app.py
@@ -17,20 +17,20 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/', methods=['GET'])
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def home():
     return "root"
 
 
 @app.route('/countries')
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def getCountries():
     countries = requests.get(URL + '/countries').json()
     return str(countries)
 
 
 @app.route('/dayone')
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def getDayOne():
     country = request.args.get('country', default='south-africa', type=str)
     dayone = requests.get(URL + '/dayone/country/' + country + '/status/' + 'confirmed').json()
@@ -38,22 +38,32 @@ def getDayOne():
 
 
 @app.route('/summary')
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def getSummary():
     summary = requests.get(URL + '/summary').json()
     return str(summary)
 
 
 @app.route('/statusbycountry')
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def getstatus():
     country = request.args.get('country', default='south-africa', type=str)
-    summary = requests.get(URL + '/country/' + country + '?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z').json()
+    summary = requests.get(URL + '/country/' + country + '?from=2020-03-02T00:00:00Z&to=2020-03-02T23:59:59Z').json()
     return str(summary)
 
 
+@app.route('/totalbycountry')
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
+def getTotal():
+    country = request.args.get('country', default='south-africa', type=str)
+    countries = requests.get(URL + '/countries').json()
+    total = []
+    for i in countries:
+            total += (requests.get(URL + '/country/' + i.get('Country') + '/status/confirmed?from=2021-03-01T00:00:00Z&to=2021-03-02T00:00:00Z')).json()
+    return str(total)
+
 @app.route('/traveldatabycountry')
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def getTravelData():
     country = request.args.get('country', default='south-africa', type=str)
     headers = {'X-Access-Token': '5cf9dfd5-3449-485e-b5ae-70a60e997864'}
@@ -62,7 +72,7 @@ def getTravelData():
 
 
 @app.route('/testsbycountry')
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def getTestData():
     country = request.args.get('country', default='south-africa', type=str)
     headers = {'X-Access-Token': '5cf9dfd5-3449-485e-b5ae-70a60e997864'}
@@ -71,7 +81,7 @@ def getTestData():
 
 
 @app.route('/whatifbycountry')
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def getWhatIfByCountry():
     country = request.args.get('country', default='south-africa', type=str)
     date = request.args.get('date', default=1, type=int)
@@ -82,7 +92,7 @@ def getWhatIfByCountry():
 
 
 @app.route('/register')
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def register():
     username = request.args.get('username', type=str)
     password = request.args.get('password', type=str)
@@ -90,7 +100,7 @@ def register():
 
 
 @app.route('/login')
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def login():
     username = request.args.get('username', type=str)
     password = request.args.get('password', type=str)
